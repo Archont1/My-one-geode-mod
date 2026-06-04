9cbcfda3a240a822640f0a2815f0be97d71a2be1
Archont1 <sevcovsvatoslav588@gmail.com>
2026-06-04 21:59:04 +0300

    Update main0.cpp

--- src/main0.cpp
+++ src/main0.cpp
@@ -4,12 +4,18 @@ using namespace geode::prelude;
 
 $on_mod(Loaded) {
     
+    static bool resetting = false;
+
     auto calculate = [] {
+        if (resetting) return; 
+
         double num0 = Mod::get()->getSettingValue<double>("num0");
         double num1 = Mod::get()->getSettingValue<double>("num1");
         int8_t op = static_cast<int8_t>(Mod::get()->getSettingValue<int64_t>("Operation"));
 
         double result = 0;
+        bool error = false;
+
         switch (op) {
             case 1: result = num0 + num1; break;
             case 2: result = num0 - num1; break;
@@ -17,21 +23,27 @@ $on_mod(Loaded) {
             case 4:
                 if (num1 == 0) {
                     Notification::create("Error: division by zero!", NotificationIcon::Error)->show();
-                    return;
+                    error = true;
                 } else {
                     result = num0 / num1;
                 }
                 break;
             default: result = 0; break;
         }
-        Notification::create(fmt::format("Result: {}", result), NotificationIcon::Info)->show();
+
+        if (!error) {
+            Notification::create(fmt::format("Result: {}", result), NotificationIcon::Info)->show();
+        }
+
+        
+        resetting = true;
+        Mod::get()->setSettingValue<double>("num0", 0.0);
+        Mod::get()->setSettingValue<double>("num1", 0.0);
+        resetting = false;
     };
 
-    
+     
     listenForSettingChanges<double>("num0", [calculate](double) { calculate(); });
     listenForSettingChanges<double>("num1", [calculate](double) { calculate(); });
     listenForSettingChanges<int64_t>("Operation", [calculate](int64_t) { calculate(); });
-    
-    Mod::get()->setSettingValue<double>("num0", 0.0);
-    Mod::get()->setSettingValue<double>("num1", 0.0);
 }
