c976770a345b973dcf54a33848c04af5ed1d5934
Archont1 <sevcovsvatoslav588@gmail.com>
2026-06-04 21:33:08 +0300

    Update main0.cpp

--- src/main0.cpp
+++ src/main0.cpp
@@ -31,4 +31,8 @@ $on_mod(Loaded) {
     listenForSettingChanges<double>("num0", [calculate](double) { calculate(); });
     listenForSettingChanges<double>("num1", [calculate](double) { calculate(); });
     listenForSettingChanges<int64_t>("Operation", [calculate](int64_t) { calculate(); });
+
+    Mod::get()->setSettingValue<int>("Operation", 0);
+    Mod::get()->setSettingValue<double>("num0", 0.0);
+    Mod::get()->setSettingValue<double>("num1", 0.0);
 }
